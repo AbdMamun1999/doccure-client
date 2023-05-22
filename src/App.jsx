b97055430180fa15f12b9c1c415f1058b08b0833
Abdullah Al Mamun <mamun12121999@gmail.com>
2023-05-22 16:46:56 +0600

    added and hook for login user exist

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { Route, RouterProvider, Routes } from "react-router-dom";
 // import Home from "./pages/main/Home";
 import routes from "./routes/routes";
+import useAuthCheck from "./hooks/useAuthCheck";
 
 function App() {
+  const authChecked = useAuthCheck();
   return <RouterProvider router={routes} />;
 }
 
